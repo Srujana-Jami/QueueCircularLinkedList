923dd77016233b2e2171a4b5a7d4fd46b4a14937
Srujana-Jami <srujana.jami@innroad.com>
2020-08-05 15:47:13 +0530

    Add project files.

---
+++ QueueCircularLinkedList/Node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QueueCircularLinkedList
+{
+    class Node
+    {
+        public int info;
+        public Node link;
+
+        public Node(int i)
+        {
+            info = i;
+            link = null;
+        }
+
+
+
+    }
+}
